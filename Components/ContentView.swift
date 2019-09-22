2754c3d6e89f885402faa62b263991b485376cc7
Christian Ampe <christianampe@icloud.com>
2019-09-22 09:16:02 -0700

    Initial Commit

---
+++ Components/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Components
+//
+//  Created by Christian Ampe on 9/22/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
