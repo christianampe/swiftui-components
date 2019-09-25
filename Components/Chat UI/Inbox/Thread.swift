e9eb1c8637bf7078a45826317042673688c6c050
Christian Ampe <christianampe@icloud.com>
2019-09-24 22:37:31 -0700

    Began building out chat components.

---
+++ Components/Chat UI/Inbox/Thread.swift
@@ -0,0 +1,15 @@
+//
+//  Thread.swift
+//  Components
+//
+//  Created by Christian Ampe on 9/24/19.
+//
+
+import Foundation
+
+struct Thread: Hashable, Codable {
+    var id: String
+    var title: String
+    var detail: String
+    var info: String
+}
---
+++ Components/Chat UI/Thread/Message.swift
@@ -0,0 +1,51 @@
+//
+//  Message.swift
+//  Components
+//
+//  Created by Christian Ampe on 9/24/19.
+//
+
+import Foundation
+
+struct Message: Hashable, Codable {
+    var id: String
+    var content: Content
+}
+
+extension Message {
+    enum Content {
+        case text(String)
+    }
+}
+
+extension Message.Content: Hashable, Codable {
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        
+        if let value = try? values.decode(String.self, forKey: .text) {
+            self = .text(value)
+            return
+        } else {
+            throw CodingError.decoding
+        }
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        
+        switch self {
+        case .text(let text):
+            try container.encode(text, forKey: .text)
+        }
+    }
+}
+
+extension Message.Content {
+    private enum CodingKeys: String, CodingKey {
+        case text
+    }
+    
+    private enum CodingError: Error {
+        case decoding
+    }
+}
